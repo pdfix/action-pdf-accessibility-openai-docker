ae0be28b709770e79cd49e4da0904c3e37e5d794
Peter Kral <peter.kral@pdfix.net>
2025-06-20 12:23:19 +0200

    PVQ-3973 External Actions - License-less runs fails

--- src/main.py
+++ src/main.py
@@ -38,7 +38,7 @@ def set_arguments(
             case "input":
                 parser.add_argument("--input", "-i", type=str, required=True, help=f"The input {file_type} file")
             case "key":
-                parser.add_argument("--key", type=str, help="PDFix license key")
+                parser.add_argument("--key", type=str, default="", nargs="?", help="PDFix license key")
             case "lang":
                 parser.add_argument("--lang", type=str, default=DEFAULT_LANG, help="Language setting")
             case "mathml-version":
@@ -50,7 +50,7 @@ def set_arguments(
                     help="MathML version",
                 )
             case "name":
-                parser.add_argument("--name", type=str, help="PDFix license name")
+                parser.add_argument("--name", type=str, default="", nargs="?", help="PDFix license name")
             case "openai-key":
                 parser.add_argument("--openai-key", type=str, required=True, help="OpenAI API key")
             case "output":
